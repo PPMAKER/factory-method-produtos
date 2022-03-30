0782ad42e4ceb6a9bf10e2b043564cc65634bb00
Pedro Paulo Galvão <52977365+PPMAKER@users.noreply.github.com>
2022-03-29 23:14:20 -0300

    Add files via upload

---
+++ exportador-simple-factory/src/main/java/com/ppmaker/exportador/AbstractColuna.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ppmaker.exportador;
+
+import java.util.function.Function;
+/**
+ *
+ * @author Pedro Paulo
+ */
+public abstract class AbstractColuna implements Coluna{
+    private Function<Produto, Object> obtemValorColuna;
+    private String titulo;
+
+    public AbstractColuna(Function<Produto, Object> obtemValorColuna, String titulo) {
+        this.obtemValorColuna = obtemValorColuna;
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String getTitulo() {
+        return titulo;
+    }
+
+    @Override
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String exportarCabecalho() {
+        return abrir() + titulo + fechar();
+    }
+
+    @Override
+    public String exportarDado(Produto produto) {
+        Object retorno = obtemValorColuna.apply(produto);
+        return abrir() + retorno.toString() + fechar();
+    }
+    
+    
+    
+    
+}
---
+++ exportador-simple-factory/src/main/java/com/ppmaker/exportador/ColunaMarkdown.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ppmaker.exportador;
+
+import java.util.function.Function;
+/**
+ *
+ * @author Pedro Paulo
+ */
+public class ColunaMarkdown extends AbstractColuna{
+    public ColunaMarkdown(Function<Produto, Object> obtemValorColuna, String titulo) {
+        super(obtemValorColuna, titulo);
+    }
+
+    @Override
+    public String abrir() {
+        return " | ";
+    }
+
+    @Override
+    public String fechar() {
+        return "";
+    }
+}
